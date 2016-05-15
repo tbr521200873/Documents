df1e47e4a9dfad8c1cdc20b5a240dfc16162f67d
tbr521200873 <346658984@qq.com>
2016-05-15 14:25:49 +0800

    d

--- Bat/Orcle_Close.bat
+++ Bat/Orcle_Close.bat
@@ -1,6 +1,7 @@
 @net stop OracleVssWriterORCL
 @net stop OracleDBConsoleorcl
 @net stop OracleJobSchedulerORCL
+@net stop OracleMTSRecoveryService
 @net stop OracleOraDb11g_home1ClrAgent
 @net stop OracleOraDb11g_home1TNSListener
 @net stop OracleServiceORCL
--- Bat/Orcle_Start.bat
+++ Bat/Orcle_Start.bat
@@ -1,6 +1,7 @@
 @net start OracleVssWriterORCL
 @net start OracleDBConsoleorcl
 @net start OracleJobSchedulerORCL
+@net start OracleMTSRecoveryService
 @net start OracleOraDb11g_home1ClrAgent
 @net start OracleOraDb11g_home1TNSListener
 @net start OracleServiceORCL
